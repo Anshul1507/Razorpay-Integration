50bf6f7f81ac9fb9b4354da9f72952da8d187344
Anshul1507 <ag1507anshul@gmail.com>
2020-04-20 20:59:00 +0530

    Added integration of razorpay --demo

--- app/src/main/java/com/netlify/anshulgupta/razorpayintegration/MainActivity.java
+++ app/src/main/java/com/netlify/anshulgupta/razorpayintegration/MainActivity.java
@@ -1,14 +1,75 @@
 package com.netlify.anshulgupta.razorpayintegration;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PaymentResultListener {
+
+    private static final String TAG = "MainActivity";
+    Button btnPay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Checkout.preload(getApplicationContext());
+
+        btnPay = findViewById(R.id.btn_pay);
+
+        btnPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startPayment();
+            }
+        });
+    }
+
+    public void startPayment() {
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            options.put("currency", "INR");
+            options.put("amount","100");
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "test@razorpay.com");
+            preFill.put("contact", "6395825922");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this,"Payment Succeed.",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this,"Payment Failed.",Toast.LENGTH_SHORT).show();
     }
 }
